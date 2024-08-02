d7da3f63ab5ddf701b3fa84047450001cec61ab1
HyeJeong0620 <catlqweasd0620@gmail.com>
2024-08-03 02:00:08 +0900

    초기 커밋.

---
+++ project/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(315, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 25);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(318, 160);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(185, 25);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(315, 220);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(188, 25);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(315, 280);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(188, 25);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(318, 340);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(185, 25);
+            this.textBox5.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 11F);
+            this.label1.Location = new System.Drawing.Point(318, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 19);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "풍향값";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 11F);
+            this.label2.Location = new System.Drawing.Point(318, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 19);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "풍속값";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 11F);
+            this.label3.Location = new System.Drawing.Point(318, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 19);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "우량값";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 11F);
+            this.label4.Location = new System.Drawing.Point(318, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 19);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "온도값";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("굴림", 11F);
+            this.label5.Location = new System.Drawing.Point(318, 316);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 19);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "습도값";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(545, 9);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(243, 266);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("굴림", 11F);
+            this.button1.Location = new System.Drawing.Point(318, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 55);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "옷 추천";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("굴림", 11F);
+            this.label6.Location = new System.Drawing.Point(314, 32);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 19);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "옷 추천";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("굴림", 11F);
+            this.button2.Location = new System.Drawing.Point(411, 380);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 56);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::project.Properties.Resources.person;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 15);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(280, 420);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // chart1
+            // 
+            chartArea1.AxisY.Maximum = 35D;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(545, 285);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            series1.Legend = "Legend1";
+            series1.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Diamond;
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(243, 151);
+            this.chart1.TabIndex = 15;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ project/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//네임스페이스추가(설치해야함)
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+//JSON
+using Newtonsoft.Json.Linq;
+using MySql.Data.MySqlClient;
+
+namespace project
+{
+    public partial class Form1 : Form
+    {
+        string Conn = "Server=localhost;Database=new_schema;Uid=root;Pwd=060620;";
+
+        //전역으로 클래스선언
+        MqttClient client;
+        string clientId;
+        string humi;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //프로그램이 실행되면 자동으로 브로커와 연결하겠다
+            //MQTT 브로커와 연결하는 부분
+            string BrokerAddress = "broker.mqtt-dashboard.com";
+            client = new MqttClient(BrokerAddress);
+
+            //구독신청을 해서 MQTT
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            // use a unique id as client id, each time we start the application
+            clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+
+            //서버와 클라이언트가 접속이 완료되는 지점
+            //IoT보드가 publish하는 topic을 subscribe해야한다
+            //Subscribe Topic 추가
+
+            string[] mytopic =
+            {
+                "bssm_iot/outdoor/weather",
+            };
+
+            byte[] myqos =
+            {
+                0,
+            };
+
+            //구독신청완료
+            client.Subscribe(mytopic, myqos);
+        }
+
+
+        //MQTT이벤트 핸들러(메세지 수신부)
+        void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            string ReceivedMessage = Encoding.UTF8.GetString(e.Message);
+
+            //DO SOMETHING..!
+            richTextBox1.Text += $"[{e.Topic}]" + ReceivedMessage + "\n";
+
+            //e.Topic
+            if (e.Topic == "bssm_iot/outdoor/weather")
+            {
+                //JSON테이터 parse
+                try
+                {
+                    //만약 json데이터가 불완전하다면 예외가 발생한다
+                    JObject myjson = JObject.Parse(ReceivedMessage);
+                    humi = myjson["humi"].ToString();
+                    textBox1.Text =  myjson["wind_direct"].ToString();
+                    textBox2.Text =  myjson["wind_speed"].ToString();
+                    textBox3.Text =  myjson["rain_amout"].ToString();
+                    textBox4.Text =  humi;
+                    textBox5.Text =  myjson["temp"].ToString();
+
+                    //삽입구문
+                    using (MySqlConnection conn = new MySqlConnection(Conn))
+                    {
+                        conn.Open();
+                        MySqlCommand msc = new MySqlCommand($"insert into weather(wind_direct,wind_speed,rain_amout,humi,temp) values({myjson["wind_direct"].ToString()},{myjson["wind_speed"].ToString()},{myjson["rain_amout"].ToString()},{myjson["humi"].ToString()},{myjson["temp"].ToString()});", conn);
+                        //INSERT into new_schema.weather(wind_direct,wind_speed,rain_amout,humi,temp) value(1,2,3,4,5);
+                        //MySqlCommand msc = new MySqlCommand("INSERT into new_schema.weather(wind_direct,wind_speed,rain_amout,humi,temp) value(1,2,3,4,5);");
+                        msc.ExecuteNonQuery();
+                    }
+
+                    double temp = double.Parse(myjson["temp"].ToString());
+
+
+                    if (chart1.Series[0].Points.Count > 10)
+                    {
+                        //제일 처음 데이터를 삭제한다
+                        chart1.Series[0].Points.RemoveAt(0);
+                    }
+
+                    chart1.Series[0].Points.Add(temp);
+                }
+                catch
+                {
+
+                }
+            }
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //유저가 프로그램을 종료했다
+            //그러면 MQTT도 종료하겠다
+            client.Disconnect();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // humi 값이 null이 아니거나 비어 있지 않은지 확인
+            if (!string.IsNullOrEmpty(humi))
+            {
+                // humi를 double로 파싱 (숫자 값을 나타낸다고 가정)
+                if (double.TryParse(humi, out double humiValue))
+                {
+                    // 임계값 설정 (이 값을 조정할 수 있음)
+                    double data1 = 28.0;
+                    double data2 = 23.0;
+                    double data3 = 20.0;
+                    double data4 = 17.0;
+                    double data5 = 12.0;
+                    double data6 = 9.0;
+                    double data7 = 5.0;
+                    double data8 = 1.0;
+
+                    // humiValue를 임계값과 비교
+                    if (humiValue >= data1) // 온도 28℃ 이상
+                    {
+                        label6.Text = "민소매";
+                        pictureBox1.Image = Properties.Resources.person21;
+                    }
+                    else if (humiValue >= data2) // 온도 23 ~ 27℃ 
+                    {
+                        label6.Text = "반팔";
+                        pictureBox1.Image = Properties.Resources.pic3;
+                    }
+                    else if (humiValue >= data3) // 온도 20 ~ 22℃ 
+                    {
+                        label6.Text = "긴팔";
+                        pictureBox1.Image = Properties.Resources.pic4;
+                    }
+                    else if (humiValue >= data4) // 온도 17 ~ 19℃ 
+                    {
+                        label6.Text = "가디건";
+                        pictureBox1.Image = Properties.Resources.pic5;
+                    }
+                    else if (humiValue >= data5) // 온도 12 ~ 16℃ 
+                    {
+                        label6.Text = "자켓";
+                        pictureBox1.Image = Properties.Resources.pic6;
+                    }
+                    else if (humiValue >= data6)
+                    {
+                        label6.Text = "목도리";
+                        pictureBox1.Image = Properties.Resources.pic7;
+                    }
+                    else if (humiValue >= data7)
+                    {
+                        label6.Text = "니트";
+                        pictureBox1.Image = Properties.Resources.pic8;
+                    }
+                    else if (humiValue >= data8)
+                    {
+                        label6.Text = "롱패딩";
+                        pictureBox1.Image = Properties.Resources.pic9;
+                    }
+                    else
+                    {
+                        // humi를 double로 파싱할 수 없는 경우 처리
+                        MessageBox.Show("잘못된 humi 값");
+                        pictureBox1.Image = Properties.Resources.person;
+                    }
+                }
+                else
+                {
+                    // humi가 null이거나 비어 있는 경우 처리
+                    MessageBox.Show("humi 값이 사용할 수 없습니다");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label6.Text = "옷";
+            pictureBox1.Image = Properties.Resources.person;
+        }
+    }
+}
